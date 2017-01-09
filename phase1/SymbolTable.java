a43bfeaebac9e794427d75ebfadf6ec0c3ea6e7f
Jayson Rook <rookjc@users.noreply.github.com>
2017-01-08 21:14:12 -0500

    Update SymbolTable.java

--- phase1/SymbolTable.java
+++ phase1/SymbolTable.java
@@ -12,10 +12,14 @@ class SymbolTable
             this.initValue = null;
         }
             
-        public Symbol( String id, int type, Object value ) {
+       /* public Symbol( String id, int type, Object value ) {
             this.id = id;
             this.type = type;
             this.initValue = value;
+        }*/
+        
+        public String getId() {
+            return this.id;
         }
     }
     
@@ -26,14 +30,18 @@ class SymbolTable
         st = new Vector<>();
     }
     
-    public void addItem( Token token )
+    public void addItem( Token token, int type )
     {
-        st.add( token.getId() );
+        st.add( new Symbol( token.getID(), type) );
     }
     
     public boolean checkSTforItem( String id )
     {
-       return st.contains( id );
+       for (Symbol s : st) {
+           if (s.getId().equals(id))
+               return true;
+       }
+       return false;
     }
 
 }
