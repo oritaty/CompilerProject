d82ccc0dd1481d1c6fa195fc6e66fb6234fc7113
Jayson Rook <rookjc@users.noreply.github.com>
2017-01-08 21:07:38 -0500

    Started modifying SymbolTable to include types and values

--- phase1/SymbolTable.java
+++ phase1/SymbolTable.java
@@ -2,12 +2,28 @@ import java.util.Vector;
 
 class SymbolTable
 {
-        
-    private Vector st;
+    private static class Symbol {
+        String id;
+        int type;
+        Object initValue; // null if not initialized in declaration
+        public Symbol( String id, int type ) {
+            this.id = id;
+            this.type = type;
+            this.initValue = null;
+        }
+            
+        public Symbol( String id, int type, Object value ) {
+            this.id = id;
+            this.type = type;
+            this.initValue = value;
+        }
+    }
+    
+    private Vector<Symbol> st;
     
     public SymbolTable()
     {
-        st = new Vector();
+        st = new Vector<>();
     }
     
     public void addItem( Token token )
